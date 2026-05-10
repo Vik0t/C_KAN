2dc006f840da144a787b4ede5caa1846032fabc5
Vik0t <viktorvarrar@gmail.com>
2026-05-10 14:53:14 +0700

    small train enhancement

--- src/training/trainer.c
+++ src/training/trainer.c
@@ -6,25 +6,45 @@
 #include <stdio.h>
 #include "loss.h"
 
-void train(Model* model, Dataset* data, int epochs, double lr) {
-    for (int e = 0; e < epochs; e++) {
-        double pred[data->size];
-        double y[data->size];
-        for (int i = 0; i < data->size; i++) {
-            double x[1];
+void train_epoch(Model* model, Dataset* data, double lr) {
+    double pred[data->size];
+    double y[data->size];
+    for (int i = 0; i < data->size; i++) {
+        double x[1];
 
-            x[0] = data->X[0][i];
-            y[i] = data->Y[0][i];
+        x[0] = data->X[0][i];
+        y[i] = data->Y[0][i];
 
-            model->forward(model->impl, x, &pred[i]);
+        model->forward(model->impl, x, &pred[i]);
 
-            double grad[1];
-            grad[0] = pred[i] - y[i];
+        double grad[1];
+        grad[0] = pred[i] - y[i];
 
-            model->backward(model->impl, x,  grad);
-            model->update(model->impl, lr);
-
-        }
-        printf("epoch: %d, loss: %f \n", e, mse(pred, y, data->size));
+        model->backward(model->impl, x,  grad);
+        model->update(model->impl, lr);
     }
 }
+
+double validate(Model* model, Dataset* data, double(*loss)(const double*, const double*, int)) {
+    double pred[data->size];
+    double y[data->size];
+    double x[1];
+    for (int i = 0; i < data->size; i++) {
+        x[0] = data->X[0][i];
+        y[i] = data->Y[0][i];
+
+        model->forward(model->impl, x, &pred[i]);
+    }
+    return loss(y, pred, data->size);
+
+}
+
+
+void train(Model* model, Dataset* data, int epochs, double lr) {
+    for (int e = 0; e < epochs; e++) {
+        train_epoch(model, data, lr);
+        double loss = validate(model, data, mse);
+        printf("epoch:%3d  loss: %5f\n", e, loss);
+    }
+}
+
